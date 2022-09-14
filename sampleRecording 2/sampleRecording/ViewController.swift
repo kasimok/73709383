78ae259ecdc16761cc8f805833e13ff9b9695f91
kakaiikaka <kasimok@163.com>
2022-09-14 08:20:29 +0800

    example

---
+++ sampleRecording 2/sampleRecording/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  sampleRecording
+//
+//  Created by kasimok on 2022-09-13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
